dbdb17ed2aa53e1fd005119c382661b7e5ff5519
Matt Watier <mattwatier@gmail.com>
2019-04-02 18:20:23 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="about p_4">
-    <h1>About the Customer Service Tool</h1>
-    <p class="font_3 color_acc line-height_3">This is a helper uitilty to quickly find, diagnose and rememdy many member data issuses that arrise at the conferences that the college hosts. </p>
-    <h3>Features Include</h3>
-    <ul>
-      <li>Find User</li>
-      <li>Password Reset</li>
-      <li>Badge Swapping</li>
-      <li>etc...</li>
-    </ul>
-  </div>
+	<div class="about p_4">
+		<h1>About the Customer Service Tool</h1>
+		<p
+			class="font_3 color_acc line-height_3"
+		>This is a helper uitilty to quickly find, diagnose and rememdy many member data issuses that arrise at the conferences that the college hosts.</p>
+		<h3>Features Include</h3>
+		<ul>
+			<li>Find User</li>
+			<li>Password Reset</li>
+			<li>Badge Swapping</li>
+			<li>etc...</li>
+		</ul>
+	</div>
 </template>
